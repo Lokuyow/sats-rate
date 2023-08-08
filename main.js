8815b6a2ad45be903a972ffb655c6de3b0897b6b
Lokuyow <cg.magnet@gmail.com>
2023-08-09 07:56:28 +0900

    refactor: main.js

--- main.js
+++ main.js
@@ -1,141 +1,164 @@
-let btcToJpy;
-let btcToUsd;
-let lastUpdatedField;
 const satsInBtc = 1e8;
+const inputFields = ['btc', 'sats', 'jpy', 'usd'];
+let btcToJpy, btcToUsd, lastUpdatedField;
 
-window.onload = async function () {
+document.addEventListener('DOMContentLoaded', initializeApp);
+
+async function initializeApp() {
     try {
-        const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=jpy%2Cusd&include_last_updated_at=true");
-        const data = await response.json();
-        btcToJpy = data.bitcoin.jpy;
-        btcToUsd = data.bitcoin.usd;
-        const updatedAt = new Date(data.bitcoin.last_updated_at * 1000);
-        document.getElementById('last-updated').textContent = `データ取得：${updatedAt.toLocaleString()}`;
+        await fetchDataFromCoinGecko();
+        setupEventListeners();
+        handleServiceWorker();
+        loadValuesFromQueryParams();
     } catch (err) {
-        console.error("Failed to fetch price data from CoinGecko:", err);
-        alert("価格データの取得に失敗しました。しばらく時間をおいてからページをリロードしてみてください。");
-        return;
+        handleError(err);
     }
+}
 
-    document.getElementById('sats').value = 100;
-    calculateValues('sats');
+async function fetchDataFromCoinGecko() {
+    const data = await getCoinGeckoData();
+    updateCurrencyRates(data);
+    updateLastUpdated(data.bitcoin.last_updated_at);
+    setDefaultValues();
+}
 
-    document.getElementById('btc').addEventListener('focus', function () {
-        this.select();
-    });
-    document.getElementById('sats').addEventListener('focus', function () {
-        this.select();
-    });
-    document.getElementById('jpy').addEventListener('focus', function () {
-        this.select();
-    });
-    document.getElementById('usd').addEventListener('focus', function () {
-        this.select();
-    });
+async function getCoinGeckoData() {
+    const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=jpy%2Cusd&include_last_updated_at=true");
+    return response.json();
+}
 
-    document.getElementById('btc').addEventListener('keyup', function () {
-        addCommasToInput(this);
-    });
-    document.getElementById('sats').addEventListener('keyup', function () {
-        addCommasToInput(this);
-    });
-    document.getElementById('jpy').addEventListener('keyup', function () {
-        addCommasToInput(this);
-    });
-    document.getElementById('usd').addEventListener('keyup', function () {
-        addCommasToInput(this);
-    });
-
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./sw.js').then((reg) => {
-            reg.addEventListener('updatefound', () => {
-                const newWorker = reg.installing;
-
-                newWorker.addEventListener('statechange', () => {
-                    if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-                        notifyUserOfUpdate(reg);
-                    }
-                });
-
-            });
-        });
-    }
-    function notifyUserOfUpdate(reg) {
-        const updateNotice = document.createElement('div');
-        updateNotice.className = 'update-notice';
-        updateNotice.innerHTML = '新しいバージョンが利用可能です<button id="updateBtn">更新</button>';
-        document.body.appendChild(updateNotice);
-        document.getElementById('updateBtn').addEventListener('click', () => {
-            if (reg.waiting) {
-                reg.waiting.postMessage('skipWaiting');
-                reg.waiting.addEventListener('statechange', () => {
-                    if (reg.waiting == null) {
-                        window.location.reload();
-                    }
-                });
-            } else {
-                console.warn('Service Worker is not waiting.');
-            }
-        });
-    }
-
-    // URLクエリパラメータ
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has('btc')) {
-        document.getElementById('btc').value = addCommas(urlParams.get('btc'));
-        calculateValues('btc');
-    }
-    if (urlParams.has('sats')) {
-        document.getElementById('sats').value = addCommas(urlParams.get('sats'));
+function setDefaultValues() {
+    const satsField = document.getElementById('sats');
+    if (!satsField.value) {
+        satsField.value = addCommas("100");
         calculateValues('sats');
     }
-    if (urlParams.has('jpy')) {
-        document.getElementById('jpy').value = addCommas(urlParams.get('jpy'));
-        calculateValues('jpy');
-    }
-    if (urlParams.has('usd')) {
-        document.getElementById('usd').value = addCommas(urlParams.get('usd'));
-        calculateValues('usd');
-    }
+}
+
+function updateCurrencyRates(data) {
+    btcToJpy = data.bitcoin.jpy;
+    btcToUsd = data.bitcoin.usd;
+}
+
+function updateLastUpdated(timestamp) {
+    const updatedAt = new Date(timestamp * 1000);
+    document.getElementById('last-updated').textContent = `データ取得：${updatedAt.toLocaleString()}`;
+}
+
+function handleError(err) {
+    console.error("Failed to fetch price data from CoinGecko:", err);
+    alert("価格データの取得に失敗しました。しばらく時間をおいてからページをリロードしてみてください。");
+}
+
+function setupEventListeners() {
+    inputFields.forEach(id => {
+        const element = document.getElementById(id);
+        element.addEventListener('focus', selectInputText);
+        element.addEventListener('keyup', formatInputWithCommas);
+    });
+
+    document.getElementById('copy-to-clipboard').addEventListener('click', function (event) {
+        const values = getValuesFromElements();
+        const textToCopy = generateCopyText(values);
+        copyToClipboard(textToCopy, event);
+    });
+}
+
+function selectInputText(event) {
+    event.target.select();
+}
+
+function formatInputWithCommas(event) {
+    addCommasToInput(event.target);
+}
+
+function handleServiceWorker() {
+    if (!('serviceWorker' in navigator)) return;
+
+    navigator.serviceWorker.register('./sw.js').then(reg => {
+        reg.addEventListener('updatefound', () => {
+            const newWorker = reg.installing;
+            newWorker.addEventListener('statechange', () => {
+                if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+                    notifyUserOfUpdate(reg);
+                }
+            });
+        });
+    });
+}
+
+function notifyUserOfUpdate(reg) {
+    const updateNotice = document.createElement('div');
+    updateNotice.className = 'update-notice';
+    updateNotice.innerHTML = '新しいバージョンが利用可能です<button id="updateBtn">更新</button>';
+    document.body.appendChild(updateNotice);
+    document.getElementById('updateBtn').addEventListener('click', () => {
+        if (reg.waiting) {
+            reg.waiting.postMessage('skipWaiting');
+            reg.waiting.addEventListener('statechange', () => {
+                if (reg.waiting == null) {
+                    window.location.reload();
+                }
+            });
+        } else {
+            console.warn('Service Worker is not waiting.');
+        }
+    });
+}
+
+// URLクエリパラメータ
+function loadValuesFromQueryParams() {
+    const urlParams = new URLSearchParams(window.location.search);
+    ['btc', 'sats', 'jpy', 'usd'].forEach(field => {
+        if (urlParams.has(field)) {
+            const element = document.getElementById(field);
+            element.value = addCommas(urlParams.get(field));
+            calculateValues(field);
+        }
+    });
 }
 
 // 計算
 function calculateValues(inputField) {
-    let btc, sats, jpy, usd;
+    const values = {
+        btc: getInputValue('btc'),
+        sats: getInputValue('sats'),
+        jpy: getInputValue('jpy'),
+        usd: getInputValue('usd')
+    };
+
     switch (inputField) {
         case 'btc':
-            btc = document.getElementById('btc').value.replace(/,/g, '');
-            sats = (btc * satsInBtc).toLocaleString();
-            jpy = (btc * btcToJpy).toLocaleString();
-            usd = (btc * btcToUsd).toLocaleString();
+            values.sats = (values.btc * satsInBtc).toLocaleString();
+            values.jpy = (values.btc * btcToJpy).toLocaleString();
+            values.usd = (values.btc * btcToUsd).toLocaleString();
             break;
         case 'sats':
-            sats = document.getElementById('sats').value.replace(/,/g, '');
-            btc = (sats / satsInBtc).toFixed(8);
-            jpy = (btc * btcToJpy).toLocaleString();
-            usd = (btc * btcToUsd).toLocaleString();
+            values.btc = (values.sats / satsInBtc).toFixed(8);
+            values.jpy = (values.btc * btcToJpy).toLocaleString();
+            values.usd = (values.btc * btcToUsd).toLocaleString();
             break;
         case 'jpy':
-            jpy = document.getElementById('jpy').value.replace(/,/g, '');
-            btc = (jpy / btcToJpy).toFixed(8);
-            sats = (btc * satsInBtc).toLocaleString();
-            usd = (btc * btcToUsd).toLocaleString();
+            values.btc = (values.jpy / btcToJpy).toFixed(8);
+            values.sats = (values.btc * satsInBtc).toLocaleString();
+            values.usd = (values.btc * btcToUsd).toLocaleString();
             break;
         case 'usd':
-            usd = document.getElementById('usd').value.replace(/,/g, '');
-            btc = (usd / btcToUsd).toFixed(8);
-            sats = (btc * satsInBtc).toLocaleString();
-            jpy = (btc * btcToJpy).toLocaleString();
+            values.btc = (values.usd / btcToUsd).toFixed(8);
+            values.sats = (values.btc * satsInBtc).toLocaleString();
+            values.jpy = (values.btc * btcToJpy).toLocaleString();
             break;
+        default:
+            console.error("Unknown inputField:", inputField);
+            return;
     }
 
-    document.getElementById('btc').value = addCommas(btc);
-    document.getElementById('sats').value = addCommas(sats);
-    document.getElementById('jpy').value = addCommas(jpy);
-    document.getElementById('usd').value = addCommas(usd);
+    inputFields.forEach(id => {
+        getElementById(id).value = addCommas(values[id]);
+    });
 
     lastUpdatedField = inputField;
-    updateShareButton(btc, sats, jpy, usd, inputField);
+    updateShareButton(values.btc, values.sats, values.jpy, values.usd);
 }
 
 // カンマ追加
@@ -154,64 +177,61 @@ function addCommasToInput(inputElement) {
     inputElement.selectionEnd = caretPos;
 }
 
-// 共有ボタン
-function updateShareButton(btc, sats, jpy, usd, lastInputField) {
-    const shareText = `₿：${addCommas(btc)} BTC\n₿：${addCommas(sats)} sats\n¥：${addCommas(jpy)} 円\n$：${addCommas(usd)} ドル\nPowered by CoinGecko,`;
-
-    let queryParams;
-    switch (lastInputField) {
+function getQueryStringFromValues(values) {
+    let queryParams = '';
+    switch (lastUpdatedField) {
         case 'btc':
-            queryParams = `?btc=${btc}`;
+            queryParams = `?btc=${values.btc.replace(/,/g, '')}`;
             break;
         case 'sats':
-            queryParams = `?sats=${sats}`;
+            queryParams = `?sats=${values.sats.replace(/,/g, '')}`;
             break;
         case 'jpy':
-            queryParams = `?jpy=${jpy}`;
+            queryParams = `?jpy=${values.jpy.replace(/,/g, '')}`;
             break;
         case 'usd':
-            queryParams = `?usd=${usd}`;
+            queryParams = `?usd=${values.usd.replace(/,/g, '')}`;
             break;
-        default:
-            queryParams = '';
     }
-    const shareUrl = "https://lokuyow.github.io/sats-rate/" + queryParams;
+    return queryParams;
+}
 
-    document.getElementById('share-twitter').href = "https://twitter.com/share?url=" + encodeURIComponent(shareUrl) + "&text=" + encodeURIComponent(shareText);
-    document.getElementById('share-nostter').href = "https://nostter.vercel.app/post?content=" + encodeURIComponent(shareText) + "%20" + encodeURIComponent(shareUrl);
-    document.getElementById('share-mass-driver').href = "https://mdrv.shino3.net/?intent=" + encodeURIComponent(shareText) + "%20" + encodeURIComponent(shareUrl);
+// 共有ボタン
+function generateShareLinks(queryParams, shareText) {
+    const shareUrl = "https://lokuyow.github.io/sats-rate/" + queryParams;
+    return {
+        twitter: "https://twitter.com/share?url=" + encodeURIComponent(shareUrl) + "&text=" + encodeURIComponent(shareText),
+        nostter: "https://nostter.vercel.app/post?content=" + encodeURIComponent(shareText) + "%20" + encodeURIComponent(shareUrl),
+        massDriver: "https://mdrv.shino3.net/?intent=" + encodeURIComponent(shareText) + "%20" + encodeURIComponent(shareUrl)
+    };
+}
+function updateShareButton(btc, sats, jpy, usd) {
+    const shareText = `₿：${addCommas(btc)} BTC\n₿：${addCommas(sats)} sats\n¥：${addCommas(jpy)} 円\n$：${addCommas(usd)} ドル\nPowered by CoinGecko,`;
+    const queryParams = getQueryStringFromValues({ btc, sats, jpy, usd });
+    const links = generateShareLinks(queryParams, shareText);
+
+    document.getElementById('share-twitter').href = links.twitter;
+    document.getElementById('share-nostter').href = links.nostter;
+    document.getElementById('share-mass-driver').href = links.massDriver;
 }
 
 // クリップボードにコピー
-document.getElementById('copy-to-clipboard').addEventListener('click', function (event) {
-    const btc = addCommas(document.getElementById('btc').value);
-    const sats = addCommas(document.getElementById('sats').value);
-    const jpy = addCommas(document.getElementById('jpy').value);
-    const usd = addCommas(document.getElementById('usd').value);
+function getValuesFromElements() {
+    return {
+        btc: addCommas(document.getElementById('btc').value),
+        sats: addCommas(document.getElementById('sats').value),
+        jpy: addCommas(document.getElementById('jpy').value),
+        usd: addCommas(document.getElementById('usd').value)
+    };
+}
 
-    let queryParams;
-    switch (lastUpdatedField) {
-        case 'btc':
-            queryParams = `?btc=${btc.replace(/,/g, '')}`;
-            break;
-        case 'sats':
-            queryParams = `?sats=${sats.replace(/,/g, '')}`;
-            break;
-        case 'jpy':
-            queryParams = `?jpy=${jpy.replace(/,/g, '')}`;
-            break;
-        case 'usd':
-            queryParams = `?usd=${usd.replace(/,/g, '')}`;
-            break;
-        default:
-            queryParams = '';
-    }
+function generateCopyText(values) {
+    const queryParams = getQueryStringFromValues(values);
+    return `₿：${values.btc} BTC\n₿：${values.sats} sats\n¥：${values.jpy} 円\n$：${values.usd} ドル\nPowered by CoinGecko, https://lokuyow.github.io/sats-rate/${queryParams}`;
+}
 
-    const shareUrl = "https://lokuyow.github.io/sats-rate/" + queryParams;
-
-    const textToCopy = `₿：${btc} BTC\n₿：${sats} sats\n¥：${jpy} 円\n$：${usd} ドル\nPowered by CoinGecko, ${shareUrl}`;
-
-    navigator.clipboard.writeText(textToCopy).then(() => {
+function copyToClipboard(text, event) {
+    navigator.clipboard.writeText(text).then(() => {
         const notification = document.getElementById('notification');
         notification.textContent = 'クリップボードにコピーしました';
         notification.style.left = event.clientX + 'px';
@@ -224,4 +244,12 @@ document.getElementById('copy-to-clipboard').addEventListener('click', function 
     }).catch(err => {
         console.error('クリップボードへのコピーに失敗しました', err);
     });
-});
+}
+
+function getElementById(id) {
+    return document.getElementById(id);
+}
+
+function getInputValue(id) {
+    return getElementById(id).value.replace(/,/g, '');
+}
