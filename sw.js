32e948ebfcacc82f02deb265ed10ab437fd82dd7
Lokuyow <94349922+Lokuyow@users.noreply.github.com>
2023-11-27 11:35:08 +0900

    fix: zap window collar

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@ const urlsToCache = [
     './images/angle-down-solid.svg'
 ];
 
-const VERSION = '1.40.1';
+const VERSION = '1.40.2';
 let CACHE_NAME = 'sats-rate-caches-' + VERSION;
 const MY_CACHES = new Set([CACHE_NAME]);
 
