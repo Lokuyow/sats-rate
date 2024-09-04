b3b59f99b5783b8ea55b0903c91ce7f8deb1eaee
Lokuyow <94349922+Lokuyow@users.noreply.github.com>
2024-09-04 23:41:52 +0900

    v2.13

--- sw.js
+++ sw.js
@@ -106,7 +106,7 @@ const urlsToCache = [
     './images/gold-icon.png'
 ];
 
-const VERSION = '2.12';
+const VERSION = '2.13';
 let CACHE_NAME = 'osats-caches-' + VERSION;
 const MY_CACHES = new Set([CACHE_NAME]);
 
