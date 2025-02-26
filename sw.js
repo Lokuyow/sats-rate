9e96bdf7c34008504182f9f405b56138b363b927
Lokuyow <94349922+Lokuyow@users.noreply.github.com>
2025-02-26 19:55:15 +0900

    fix: update service worker version to 2.20.1

--- sw.js
+++ sw.js
@@ -113,7 +113,7 @@ const urlsToCache = [
   "/assets/images/gold-icon.png",
 ];
 
-const VERSION = "2.20.0";
+const VERSION = "2.20.1";
 let CACHE_NAME = "osats-caches-" + VERSION;
 const MY_CACHES = new Set([CACHE_NAME]);
 
