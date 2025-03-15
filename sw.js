52f67fee3c82a1289dee42b3d1f7f7530275c3d3
Lokuyow <94349922+Lokuyow@users.noreply.github.com>
2025-03-16 08:39:42 +0900

    fix: サービスワーカーのバージョンを2.20.2に更新

--- sw.js
+++ sw.js
@@ -113,7 +113,7 @@ const urlsToCache = [
   "/assets/images/gold-icon.png",
 ];
 
-const VERSION = "2.20.1";
+const VERSION = "2.20.2";
 let CACHE_NAME = "osats-caches-" + VERSION;
 const MY_CACHES = new Set([CACHE_NAME]);
 
