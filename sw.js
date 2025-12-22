cd7b7cf428c5a8b53646e6527e5dcbd5ab7988d8
Lokuyow <94349922+Lokuyow@users.noreply.github.com>
2025-12-22 22:13:28 +0900

    バージョン番号を2.23.6から2.23.7に更新しました

--- sw.js
+++ sw.js
@@ -114,7 +114,7 @@ const urlsToCache = [
   "/assets/images/gold-icon.png",
 ];
 
-const VERSION = "2.23.6";
+const VERSION = "2.23.7";
 let CACHE_NAME = "osats-caches-" + VERSION;
 const MY_CACHES = new Set([CACHE_NAME]);
 
